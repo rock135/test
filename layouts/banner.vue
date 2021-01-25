4dd54a315a894d802ed8d75e2a3cec5c4fcf6ebc
shi <982097679@qq.com>
2021-01-25 16:17:10 +0800

    table

---
+++ layouts/banner.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="app_wrap">
+        <img class="banner" />
+        <nuxt />
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.app_wrap{
+    padding: 20px;
+}
+.app_wrap >.banner {
+  background: #13a3f1;
+  width: 100%;
+  height: 150px;
+  margin-bottom: 20px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,191 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        test
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
+  <div class="table_wrap">
+    <el-table
+      :data="tableData"
+      :header-row-style="{background: '#13a3f1'}"
+      :header-cell-style="{background: '#13a3f1', color: '#fff'}"
+      style="width: 100%;text-align: center">
+      <el-table-column
+        label="起始/中转/目的"
         >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+        <template slot-scope="scope">
+          <div class="distance_wrap">
+            <p class="add_tag1">{{ scope.row.departure }}</p>
+            <p style="color: #a2a2a2;font-size: 10px"><i class="el-icon-warning" style="color: #13a3f1"></i>{{ scope.row.transfer }}</p>
+            <p class="add_tag2">{{ scope.row.arrive }}</p>
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="20GP"
+        align="center"
+        width="180">
+        <template slot-scope="scope">
+          <span style="color: red;">{{ `$${ scope.row.price1 }` }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="40GP"
+        align="center"
+        width="180">
+        <template slot-scope="scope">
+          <span style="color: red;">{{ `$${ scope.row.price2 }` }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="40HQ"
+        align="center"
+        width="180">
+        <template slot-scope="scope">
+          <span style="color: red;">{{ `$${ scope.row.price3 }` }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="船期"
+        align="center"
+        width="220">
+        <template slot-scope="scope">
+          <span>{{ scope.row.date }}</span>
+          <p v-if="scope.row.tip" style="color: #a2a2a2;font-size: 12px">{{ scope.row.tip }}</p>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label=""
+        align="center"
+        width="180">
+        <template slot-scope="scope">
+          <el-button type="primary" @click="open(scope.row)">咨询</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
-</template>
+  </template>
 
 <script>
-export default {}
+  export default {
+    layout: 'banner',
+    head() {
+      return {
+        title: '运费查询'
+      }
+    },
+    data() {
+      return {
+        tableData: [
+          {
+            date: '周二',
+            tip: '内陆点单票价格',
+            transfer: '直达',
+            departure: '上海',
+            arrive: '北京',
+            price1: 669,
+            price2: 1269,
+            price3: 1369
+          },
+          {
+            date: '周二',
+            transfer: '直达',
+            departure: '上海',
+            arrive: '北京',
+            price1: 669,
+            price2: 1269,
+            price3: 1369
+          },
+          {
+            date: '周二',
+            transfer: '直达',
+            departure: '上海',
+            arrive: '北京',
+            price1: 669,
+            price2: 1269,
+            price3: 1369
+          },
+          {
+            date: '周二',
+            tip: '内陆点单票价格',
+            transfer: '直达',
+            departure: '上海',
+            arrive: '北京',
+            price1: 669,
+            price2: 1269,
+            price3: 1369
+          },
+          {
+            date: '周二',
+            transfer: '直达',
+            departure: '上海',
+            arrive: '北京',
+            price1: 669,
+            price2: 1269,
+            price3: 1369
+          },
+          {
+            date: '周二',
+            transfer: '直达',
+            departure: '上海',
+            arrive: '北京',
+            price1: 669,
+            price2: 1269,
+            price3: 1369
+          }
+        ]
+      }
+    },
+    methods: {
+      open(data) {
+        this.$alert(`${ data.price1 }`, '运费', {
+          confirmButtonText: '确定'
+        });
+      }
+    }
+  }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  .table_wrap {
+    width: calc(100% - 100px);
+    margin: 0 auto;
+  }
+  .distance_wrap{
+    padding-left: 20px;
+    display: flex;
+    flex-direction: column;
+  }
+  .distance_wrap::before{
+    display: block;
+    content: '';
+    height: 52px;
+    border-left: 1px solid #13a3f1;
+    position: absolute;
+    top: 20px;
+    left: 10px;
+  }
+  .distance_wrap .el-icon-warning{
+    position: absolute;
+    left: 3px;
+    top: 41px;
+    font-size: 14px;
+  }
+  .distance_wrap .add_tag1::before{
+    display: block;
+    content: '';
+    width: 7px;
+    height: 7px;
+    border-radius: 50%;
+    background: #13a3f1;
+    position: absolute;
+    top: 20px;
+    left: 7px;
+  }
+  .distance_wrap .add_tag2::before{
+    display: block;
+    content: '';
+    width: 7px;
+    height: 7px;
+    border-radius: 50%;
+    background: #13a3f1;
+    position: absolute;
+    top: 68px;
+    left: 7px;
+  }
 </style>
